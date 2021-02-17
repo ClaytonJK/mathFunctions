2fddba085f92bc2ab795af4f288f95116b26bfc7
ClaytonJK <77551168+ClaytonJK@users.noreply.github.com>
2021-02-16 20:39:16 -0500

    Added PrimesDeterminerWithList
    This does the same function as the other PrimesDeterminer, but outputs factors as a list rather than as individual print statements.  Will periodically print "Thinking..." to indicate that the program is still running. I plan on adding an abort function to manually stop long-running primes.

---
+++ PrimesDeterminerWithList.py
@@ -0,0 +1,16 @@
+
+testNumber = int(input("Please give the desired number:"))
+divisor = 2
+factorable = False
+factorsList = list()
+n = 0
+while(testNumber>divisor) : 
+    if testNumber%divisor == 0 :
+        factorsList.insert(n,divisor)
+        n += 1
+        print("Thinking....")
+    divisor += 1
+if len(factorsList) == 0 :
+    print(testNumber, "is prime")
+else :
+    print(testNumber,"is not prime with factors of", factorsList)
